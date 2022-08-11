332a1612fddbd6b94f20f5eae3a77aa4fafe0616
SVWEB01 <110264471+SVWEB01@users.noreply.github.com>
2022-08-11 07:47:55 -0600

    Add files via upload

---
+++ Registro/php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php 
+
+$conexion = mysqli_connect("localhost", "id18009843_root", "(AnderGamer1)", "id18009843_login_register_db");
+
+/*
+if($conexion){
+    echo 'Conectado exitosamente a la Base de Datos';
+}else{
+    echo 'No se a podido conecta a la base de datos';
+}
+*/
+
+?>
